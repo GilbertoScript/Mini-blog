15bc1369a0ffd6520446397ba15ae9116c02a565
GilbertoASJ <alvesgilberto84@gmail.com>
2022-07-01 16:47:12 -0300

    Feature: :sparkles: Adding css navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Footer = () => {
 
 	return (
-		<footer>
+		<footer className={styles.footer}>
 			<h3>Escreva sobre o que você tem interesse!</h3>
 			<p>Mini Blog &copy; 2022</p>
 		</footer>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,21 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
 
 	return (
-		<nav>
-			<NavLink to="/">
+		<nav className={styles.navbar}>
+			<NavLink to="/" className={styles.brand}>
 				Mini <span>Blog</span>
 			</NavLink>
 
-			<ul>
+			<ul className={styles.links_list}>
 				<li>
-					<NavLink to="/">Home</NavLink>
+					<NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>
+						Home
+					</NavLink>
 				</li>
 				<li>
-					<NavLink to="/about">Sobre</NavLink>
+					<NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>
+						Sobre
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
