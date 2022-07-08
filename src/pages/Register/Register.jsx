4f610d1c3b7b4ada6b672f902cd9448567c818ba
GilbertoASJ <alvesgilberto84@gmail.com>
2022-07-08 11:40:32 -0300

    Feature: :sparkles: Register forms state

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,9 +5,33 @@ import { useState, useEffect } from 'react'
 
 const Register = () => {
 
+	// States do form
+	const [displayName, setDisplayName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setError('');
+
+		const user = {
+			displayName,
+			email,
+			password
+		}
+
+		if(password !== confirmPassword) {
+			setError('As senhas precisam ser iguais!');
+			return;
+		}
+
+		console.log(user)
+	}
+
 	return (
 		<div className={styles.register}>
-
 			<div className={styles.formBox}>
 
 				<div className={styles.imageColumn}>
@@ -18,14 +42,17 @@ const Register = () => {
 					<h1>Cadastre-se para postar</h1>
 					<p>Crie seu usuário e compartilhe suas histórias!</p>
 
-					<form>
-						
+					{error && (<p className="error">{error}</p>)}
+
+					<form onSubmit={handleSubmit}>
 						<label>
 							<span>Nome:</span>
 							<input 
 								type="text" 
 								name="displayName" 
 								placeholder="Nome do usuário"
+								value={displayName}
+								onChange={(e) => {setDisplayName(e.target.value)}}
 								required 
 							/>
 						</label>
@@ -36,6 +63,8 @@ const Register = () => {
 								type="email" 
 								name="email" 
 								placeholder="Digite seu e-mail"
+								value={email}
+								onChange={(e) => {setEmail(e.target.value)}}
 								required 
 							/>
 						</label>
@@ -46,6 +75,8 @@ const Register = () => {
 								type="password" 
 								name="password" 
 								placeholder="Digite sua senha"
+								value={password}
+								onChange={(e) => {setPassword(e.target.value)}}
 								required 
 							/>
 						</label>
@@ -56,6 +87,8 @@ const Register = () => {
 								type="password" 
 								name="confirmPassword" 
 								placeholder="Confirme sua senha"
+								value={confirmPassword}
+								onChange={(e) => {setConfirmPassword(e.target.value)}}
 								required 
 							/>
 						</label>
