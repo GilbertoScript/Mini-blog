d163d7294e87ba5a3dbbe328cf54b5d6a39fbe82
GilbertoASJ <alvesgilberto84@gmail.com>
2022-07-13 12:05:13 -0300

    Feature: :sparkles: Passing user to context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.scss';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+// Hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication'
 
 // Context
 import { AuthProvider } from './context/AuthContext'
@@ -17,10 +22,27 @@ import Footer from './components/Footer/Footer';
 
 function App() {
 
+    const [user, setUser] = useState(undefined);
+    const { auth } = useAuthentication();
+
+    const loadingUser = user === undefined;
+
+    useEffect(() => {
+
+        onAuthStateChanged(auth, (user) => {
+            setUser(user)
+        })
+
+    }, [auth])
+
+    if(loadingUser) {
+        return ( <p>Carregando...</p> );
+    }
+
     return (
         <div className="App">
-            <AuthProvider>
-            
+            <AuthProvider value={{user}}>
+
                 <BrowserRouter>
                     <Navbar />
                     <main className="container">
