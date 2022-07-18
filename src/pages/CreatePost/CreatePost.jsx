d26e759c6ffbb745a49a4cd2c17075a0fc3d677d
GilbertoASJ <alvesgilberto84@gmail.com>
2022-07-18 15:28:58 -0300

    Feature: :sparkles: Creating create post page

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,11 +1,82 @@
 import styles from './CreatePost.module.scss';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+
+import SpinnerButton from '../../components/SpinnerButton/SpinnerButton';
+
 const CreatePost = () => {
 
+	// States do post
+	const [title, setTitle] = useState('');
+	const [image, setImage] = useState('');
+	const [body, setBody] = useState('');
+	const [tags, setTags] = useState([]);
+	const [formError, setFormError] = useState([]);
+
+	const handleSubmit = (e) => {
+
+		e.preventDefault();
+
+	}
+
 	return (
-		<>
-			<h1>CreatePost</h1>
-		</>
+		<div className={styles.createPost}>
+			<h2>Criar post</h2>
+			<p>Escreva sobre o que quiser, e compartilhe seu conhecimento!</p>
+
+			<form onSubmit={handleSubmit}>
+				
+				<label>
+					<span>Título: </span>
+					<input 
+						type="text" 
+						name="title" 
+						required
+						placeholder="Pense em um bom título" 
+						onChange={(e) => {setTitle(e.target.value)}}
+						value={title}
+					/>
+				</label>
+				<label>
+					<span>URL da imagem: </span>
+					<input 
+						type="text" 
+						name="image" 
+						required
+						placeholder="Insira uma imagem que representa o seu post" 
+						onChange={(e) => {setImage(e.target.value)}}
+						value={image}
+					/>
+				</label>
+				<label>
+					<span>Conteúdo do post: </span>
+					<textarea 
+						name="body" 
+						required 
+						placeholder="Insira o conteúdo do post"
+						onChange={(e) => {setBody(e.target.value)}}
+						value={body}
+					></textarea>	
+				</label>
+				<label>
+					<span>Tags para o post: </span>
+					<input 
+						type="text" 
+						name="tags" 
+						required
+						placeholder="Insira as tags separadas por vírgula" 
+						onChange={(e) => {setTags(e.target.value)}}
+						value={tags}
+					/>
+				</label>
+
+				<button className="btn">Cadastrar-se</button>
+				{/*{!loading && (<button className="btn">Cadastrar-se</button>)}
+				{loading && (<SpinnerButton />)}*/}
+			</form>
+		</div>
 	)
 }
 
