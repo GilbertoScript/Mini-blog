b9d6c6d096da8e084194f5b728ab0d3958b8a97f
GilbertoASJ <alvesgilberto84@gmail.com>
2022-08-13 11:49:00 -0300

    Feature: :sparkles: Creating css to individual post page

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,4 +1,5 @@
 import styles from './Post.module.scss';
+import imgNotFound from '../../assets/notfound.svg';
 
 import { useParams } from 'react-router-dom';
 import { useFetchSingleDocument } from '../../hooks/useFetchSingleDocuments';
@@ -9,14 +10,33 @@ const Post = () => {
 	const { document: post, loading } = useFetchSingleDocument('posts', id)
 
 	return (
-		<>
+		<div className={styles.post_container}>
 			{loading && (<p>Carregando post...</p>)}
 			{post && (
-				<>
+				<div>
 					<h1>{post.title}</h1>
-				</>
+					<img 
+						src={post.image} 
+						alt={post.title} 
+						onError={({ currentTarget }) => {
+						    currentTarget.onerror = null;
+						    currentTarget.src = `${imgNotFound}`;
+						    currentTarget.height = 300;
+						}}
+					/>
+					<p className={styles.body}>{post.body}</p>
+					<h3>Este post trata sobre:</h3>
+					<div className={styles.tags}>
+						{post.tagsArray.map((tag) => (
+
+							<p key={tag}>
+								<strong>#</strong>{tag}
+							</p>
+						))}
+					</div>
+				</div>
 			)}
-		</>
+		</div>
 	)
 }
 
