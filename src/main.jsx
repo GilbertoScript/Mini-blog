3178608ee42a754d50f5e08d3de450bdddd220d3
Gilberto Alves <alvesgilberto84@gmail.com>
2024-04-24 16:38:43 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-DC9KD9LWL2");
+
+// Send pageview with a custom path
+ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Custom Title" });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
